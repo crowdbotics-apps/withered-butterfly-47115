b31fdb7dfe8f39d787c5b3ffdf33fec4cdcd8635
Crowdbotics <team@crowdbotics.com>
2024-03-18 03:24:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-butterfly-47115",
+  url: "https://withered-butterfly-47115.botics.co",
+  api: "https://withered-butterfly-47115.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
